995fd6d3d94bd09f2c61186127f1941d0bd904ad
Amina <aminaopio@my.unt.edu>
2023-05-18 11:54:26 -0500

    Standard setup

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Amina Opio</h1>
+      <div id="page-body">
+
+        Welcome to my Portfolio!
+      </div>
+    </div>
+  );
+}
+
+export default App;
