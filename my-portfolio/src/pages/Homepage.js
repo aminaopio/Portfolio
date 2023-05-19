da766f28e39ea590feacf6dda64f935b53ad9a5e
Amina <aminaopio@my.unt.edu>
2023-05-19 11:28:16 -0500

    Fixing HomePage Styling

--- my-portfolio/src/pages/Homepage.js
+++ my-portfolio/src/pages/Homepage.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Typewriter from 'typewriter-effect';
+import "./Homepage.css";
 const HomePage = () => {
-    return (
-        <h1>This is the homepage!</h1>
-    );
+
+        const [state] = useState ({
+            title: "Hi,",
+            titleTwo: "I'm Amina. (▰˘◡˘▰) ",
+            // titleThree: "Amina",
+        });
+        return (<div className='HomePage'>
+            <div className='HomePage-intro'>
+                <h2>
+                    <div className='title'>{state.title}</div>
+                    <div className='titleTwo'>{state.titleTwo}</div>
+                    {/* <div className='titleThree'>{state.titleThree}</div> */}
+                </h2>
+                <div className="text">
+                    <Typewriter options={{
+                        autoStart:true,
+                        loop: true,
+                        delay: 40,
+                        strings: [
+                            "I'm a Developer.",
+                            "I'm a Computer Science graduate.",
+                            "I'm a Photographer.",
+                            "I'm a Digital Artist.",
+
+                        ],
+                    }}/>
+                </div>
+                <div className='contact-me'>
+                    <button className='button'>Contact Me!</button>
+                </div>
+            </div>
+        </div>) 
+    
 }
 
 export default HomePage;
