af51fb31d82a1d66d397a4e9a828435a87b98c52
Amina <aminaopio@my.unt.edu>
2023-05-25 14:03:51 -0500

    skills

--- my-portfolio/src/pages/Homepage.js
+++ my-portfolio/src/pages/Homepage.js
@@ -59,19 +59,18 @@ const HomePage = () => {
                 <ol className='list'>
                 <li className='item'>
                     <h2>Front-End</h2>
-                    <span> ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
-              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</span>
+                    <span> ReactJS, HTML, CSS, React Native, Flutter, NPM,
+              Ionic, BootStrap, MaterialUI, TailwindCSS, StyledComponents</span>
 
                 </li>
                 <li className='item'>
                     <h2>Back-End</h2>
-                    <span> NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
-              MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL</span>
+                    <span> NodeJS, Firebase, MySQL, MongoDB, DynamoDB, AWS S3, MS SQL</span>
 
                 </li>
                 <li className='item'>
                     <h2>Languages</h2>
-                    <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
+                    <span>C, C++, JavaScript, Dart, Python, TypeScript, Golang</span>
 
                 </li>
                 </ol>
